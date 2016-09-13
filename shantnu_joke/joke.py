0aa7da7e5a8dc794c8bd3364d409407f974670ca
Shantnu Tiwari <shantnu@users.noreply.github.com>
2016-09-13 11:06:44 +0100

    remove ver

--- shantnu_joke/joke.py
+++ shantnu_joke/joke.py
@@ -1,2 +1,2 @@
 def joke():
-    print("v0.3 There are 10 types of programmers: Thsoe who understand binary, adn those who don't. Also, me.")
+    print("There are 10 types of programmers: Thsoe who understand binary, adn those who don't. Also, me.")
