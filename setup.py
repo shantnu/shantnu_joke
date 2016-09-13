dca5cdbc8afdf356262a49dd0a3d5e72a16cf6fb
Shantnu Tiwari <shantnu@users.noreply.github.com>
2016-09-13 11:13:57 +0100

    Fixing metadata

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 
 setup(name='shantnu_joke',
-      version='0.3',
+      packages = ['shantnu_joke'],
+      version='0.4',
       description='Shantnus terrible joke',
       url='https://github.com/shantnu/shantnu_joke',
+      download_url = 'https://github.com/shantnu/shantnu_joke/archive/0.4.zip',
       author='Shantnu',
       license='GPL',
       )
